cc994b3404b3e5906a3611f298cccbb009b89531
AnttiHalmetoja <anttihalmetoja@gmail.com>
2020-08-10 14:57:02 +0300

    add more tables to schema

--- schema.sql
+++ schema.sql
@@ -7,13 +7,29 @@ CREATE TABLE users (
 
 CREATE TABLE answers (
     id SERIAL PRIMARY KEY,
+    answer TEXT,
     user_id INTEGER REFERENCES users,
-    answer INTEGER
+    test_id INTEGER REFERENCES choices,
+    
+);
+
+CREATE TABLE questions (
+    id SERIAL PRIMARY KEY,
+    question TEXT,
+    user_id INTEGER REFERENCES tests
+    
 );
 
 CREATE TABLE choices (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users,
-    choice TEXT
+    choice TEXT,
+    user_id INTEGER REFERENCES tests
+    
+);
+
+CREATE TABLE tests (
+    id SERIAL PRIMARY KEY,
+    topic TEXT,
+    created_at TIMESTAMP
 );
 
